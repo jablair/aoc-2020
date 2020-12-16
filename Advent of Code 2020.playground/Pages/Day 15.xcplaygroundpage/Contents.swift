ee59b29bbb005822110429a1213c553c79c6d0f3
eblair <eblair@raoli.com>
2020-12-15 22:22:09 -0500

    Day 15 - mutable state is bad

--- Advent of Code 2020.playground/Pages/Day 15.xcplaygroundpage/Contents.swift
+++ Advent of Code 2020.playground/Pages/Day 15.xcplaygroundpage/Contents.swift
@@ -2,25 +2,24 @@
 
 //: [Day 15](https://adventofcode.com/2020/day/15)
 
-
 import Foundation
 
-var input = [5,2,8,16,18,0,1]
-
-var positions: [Int: [Int]] = [:]
+let input = [5,2,8,16,18,0,1]
 
 class MemoryGame {
     private var lastIndexPerCard: [Int: Int]
     private var playCard: Int
-    let startRound: Int
+    private let startRound: Int
+    private let rounds: Int
     
-    init?(seed: [Int]) {
+    init?(seed: [Int], rounds: Int) {
         guard let position = seed.last else {
             return nil
         }
         
         self.playCard = position
         self.startRound = seed.endIndex
+        self.rounds = rounds
         
         self.lastIndexPerCard = [:]
         
@@ -29,7 +28,7 @@ class MemoryGame {
         }
     }
     
-    func play(rounds: Int) -> Int {
+    func play() -> Int {
          
         for round in startRound..<rounds {
             let newVal: Int
@@ -42,16 +41,15 @@ class MemoryGame {
             lastIndexPerCard[playCard] = round
             playCard = newVal
         }
-        
-        
+
         return playCard
     }
 }
 
-let game = MemoryGame(seed: input)!
-
 //: Problem 1
-print("Round 2020: \(game.play(rounds: 2020))")
+let game1 = MemoryGame(seed: input, rounds: 2020)!
+print("Round 2020: \(game1.play())")
 
 //: Problem 2
-print("Round 30000000: \(game.play(rounds: 30_000_000))")
+let game2 = MemoryGame(seed: input, rounds: 30_000_000)!
+print("Round 30000000: \(game2.play(rounds: 30_000_000))")
